7731a277e86a8399794e9f88d2e19789c3af239b
sabrinaZait <zsabrina941@outlook.fr>
2022-06-25 15:45:28 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+
+import React, {useState} from 'react';
+import axios from "axios"
 import './App.css';
 
+//set local storage json.stringlify 
+
 function App() {
+  // get data using axios each time the user change the type
+  // get randome activity each time the user submit 
+  // deal with favoritess
+  const [type,setType]= useState("education")
+  const [data,setData]=useState([])
+  const types = ["education", "recreational", "social", "diy", "charity", "cooking", "relaxation", "music", "busywork"]
+  // Make a request for a user with a given ID
+  const getData= async(value)=>{ 
+    console.log(type)
+    await axios.get(`http://www.boredapi.com/api/activity?type=${value}`)
+      .then(function (response) {
+        // handle success
+        console.log(response.data);
+
+        setData(response.data);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+      .then(function () {
+        // always executed
+      })
+  };
+  // useEffect(() => {
+  //   getData();
+  // },[getData]);
+  const updateType =(t)=>{
+    setType(t)
+    getData(t)
+}
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <label >type of activity : </label>
+      <select name="types" id="pet-type" onChange={e=>updateType(e.target.value)}> 
+        {types.map((t) => (
+          <option value={t} key ={t} >{t}</option>
+        ))}
+      </select>
+      <button  onClick={e=>getData(type)}>change</button>
+      <h1>{type}</h1>
+      <h1>{data.activity}</h1>
     </div>
   );
 }
